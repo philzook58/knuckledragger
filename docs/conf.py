70000fd17d5f86df7dfefe0f2823abe1d4510576
Philip Zucker <philzook58@gmail.com>
2024-12-20 18:47:07 -0500

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,13 @@ extensions = [
 ]
 autosummary_generate = True
 
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,  # Include undocumented members
+    "inherited-members": True,
+    "show-inheritance": True,
+}
+
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
--- kdrag/__init__.py
+++ kdrag/__init__.py
@@ -4,26 +4,39 @@ from . import notation
 from . import tactics
 from . import utils as utils
 
+Proof = kernel.Proof
+"""The type of Proof objects"""
 
 lemma = tactics.lemma
+"""Declare a lemma with auto generated proof. `by` keyword argument can be used to provide a list of previously proved lemmas"""
+
 axiom = kernel.axiom
+"""Declare an axiom"""
+
 define = kernel.define
-Proof = kernel.Proof
+"""Declare a definition"""
 
 QForAll = notation.QForAll
+"""Quantified forall. Auto adds well formedness `wf(x)`"""
 QExists = notation.QExists
+"""Quantified exists. Auto adds well formedness `wf(x)`"""
+
 cond = notation.cond
+"""Helper for conditional expressions (chained If)"""
+
 Record = notation.Record
+"""Declare a record type"""
 NewType = notation.NewType
-
-"""Declare datatypes with auto generated induction principles"""
+"""Declare a newtype"""
 Inductive = notation.Inductive
+"""Declare datatypes with auto generated induction principles"""
 
-"""Tactic for calculation style proofs"""
 Calc = tactics.Calc
-
+"""Tactic class for calculation style proofs"""
 Lemma = tactics.Lemma
+"""Tactic class for interactive proofs"""
 
+# TODO: Remove this
 R = smt.RealSort()
 Z = smt.IntSort()
 RSeq = Z >> R
