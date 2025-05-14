728fe777d4b0acea6ebafff0bf1256698a577427
Philip Zucker <philzook58@gmail.com>
2025-05-14 10:39:44 -0400

    small sqlite datalog

---
+++ kdrag/solvers/datalog.py
@@ -0,0 +1,107 @@
+import kdrag.smt as smt
+import kdrag.rewrite as rw
+import sqlite3
+
+"""
+
+A simple datalog bv compiling clauses to SQL.
+See snakelog https://github.com/philzook58/snakelog
+
+"""
+
+
+def _compile_body(
+    vs: list[smt.ExprRef], body: smt.BoolRef
+) -> tuple[dict, list[str], list[str]]:
+    todo = [body]
+    env = {}
+    froms = []
+    wheres = []
+    counter = 0
+    while todo:
+        rel = todo.pop()
+        if smt.is_eq(rel):
+            raise ValueError("Equality not supported")  # TODO
+        elif smt.is_not(rel):
+            raise ValueError("Negation not supported")  # TODO
+        elif smt.is_or(rel):
+            raise ValueError("Disjunction not supported")  # TODO
+        elif smt.is_and(rel):
+            todo.extend(rel.children())
+        elif smt.is_true(rel):
+            continue
+        elif smt.is_app(rel):
+            name = rel.decl().name()
+            args = rel.children()
+            row_name = name + str(counter)
+            counter += 1
+            froms.append(f"{name} AS {row_name}")
+            for n, arg in enumerate(args):
+                if arg in vs:
+                    if arg in env:
+                        wheres.append(f"{env[arg]} = {row_name}.x{n}")
+                    else:
+                        env[arg] = f"{row_name}.x{n}"
+                else:
+                    wheres.append(f"{row_name}.x{n} = {str(arg)}")
+        else:
+            raise ValueError(f"Unsupported expression: {rel}")
+    return env, froms, wheres
+
+
+def _compile_rule(rule: rw.Rule) -> str:
+    env, froms, wheres = _compile_body(rule.vs, rule.hyp)
+    name = rule.conc.decl().name()
+    selects = []
+    for n, arg in enumerate(rule.conc.children()):
+        if arg in rule.vs:
+            if arg in env:
+                selects.append(f"{env[arg]} AS x{n}")  # maybe select as keyword
+            else:
+                raise ValueError(f"Variable {arg} not found in body")
+        else:
+            selects.append(f"{arg} AS x{n}")
+    froms = ", ".join(froms)
+    wheres = " AND ".join(wheres)
+    selects = ", ".join(selects)
+    return f"""
+    INSERT OR IGNORE INTO {name} SELECT DISTINCT {selects}
+    {"FROM " + froms if froms else ""}
+    {"WHERE " + wheres if wheres else ""}
+    """
+
+
+class Datalog:
+    """
+    A simple datalog solver using sqlite
+
+    >>> import kdrag as kd
+    >>> s = Datalog()
+    >>> edge = smt.Function('edge', smt.IntSort(), smt.IntSort(), smt.BoolSort())
+    >>> path = smt.Function('path', smt.IntSort(), smt.IntSort(), smt.BoolSort())
+    >>> s.declare_sig([edge, path])
+    >>> x, y, z = smt.Ints('x y z')
+    >>> s.run(edge(1,2))
+    >>> s.run(edge(2,3))
+    >>> s.run(kd.QForAll([x,y], edge(x,y), path(x,y)))
+    >>> s.run(kd.QForAll([x,y,z], edge(x,y), path(y,z), path(x,z)))
+    >>> s.run(kd.QForAll([x,y,z], edge(x,y), path(y,z), path(x,z)))
+    >>> s.db.execute("SELECT * FROM path").fetchall()
+    [(1, 2), (2, 3), (1, 3)]
+    """
+
+    def __init__(self):
+        self.db = sqlite3.connect(":memory:")
+
+    def declare_sig(self, sig: list[smt.FuncDeclRef]):
+        for f in sig:
+            primkey = "(" + ", ".join([f"x{i}" for i in range(f.arity())]) + ")"
+            self.db.execute(f"""CREATE TABLE IF NOT EXISTS {f.name()} 
+                            ({', '.join([f'x{i} {f.range().name()}' for i in range(f.arity())])}, 
+                             PRIMARY KEY {primkey})""")
+
+    def run(self, rule: rw.Rule | smt.BoolRef):
+        if isinstance(rule, smt.BoolRef):
+            rule = rw.rule_of_expr(rule)
+        sql = _compile_rule(rule)
+        self.db.execute(sql)
--- kdrag/solvers/egraph.py
+++ kdrag/solvers/egraph.py
@@ -2,9 +2,11 @@ import kdrag.smt as smt
 from dataclasses import dataclass
 from collections import defaultdict
 import itertools
+import copy
 
 # TODO: prune on models
 # TODO: extract
+# TODO: Proofs
 
 
 @dataclass
@@ -20,6 +22,29 @@ class EGraph:
         self.uf = {}
         self.solver = smt.Solver()
 
+    def copy(self):
+        """
+        Copy the egraph. This is a shallow copy, so the terms are not copied.
+
+        >>> E = EGraph()
+        >>> f = smt.Function('f', smt.IntSort(), smt.IntSort())
+        >>> x,y,z = smt.Ints('x y z')
+        >>> E.add_term(f(x))
+        >>> E.add_term(f(y))
+        >>> E.union(x,y)
+        >>> assert E.find(f(x)) != E.find(f(y))
+        >>> E2 = E.copy()
+        >>> E2.rebuild()
+        >>> assert E2.find(f(x)) == E2.find(f(y))
+        >>> assert E.find(f(x)) != E.find(f(y))
+        """
+        new = EGraph()
+        new.roots = defaultdict(set, {k: v.copy() for k, v in self.roots.items()})
+        new.terms = self.terms.copy()
+        new.uf = self.uf.copy()
+        new.solver = copy.copy(self.solver)
+        return new
+
     def _find(self, eid: int) -> int:
         while eid in self.uf:  # invariant: root not in uf.
             eid = self.uf[eid]
@@ -32,6 +57,7 @@ class EGraph:
     def union(self, t1: smt.ExprRef, t2: smt.ExprRef) -> None:
         """
         Assert equal two terms in the EGraph.
+        Note that this does not add the terms to the EGraph.
 
         >>> x,y,z = smt.Ints('x y z')
         >>> E = EGraph()
@@ -103,7 +129,7 @@ class EGraph:
                         if res == smt.unsat:
                             self.union(t1, t2)  # don't need to solver.add but whatev
 
-    def ematch(self, sorts: list[smt.SortRef], f):
+    def rw(self, sorts: list[smt.SortRef], f):
         """
         Bottom up ematch and rewrite.
         f should take one argumentsper each sort in sorts
@@ -113,7 +139,7 @@ class EGraph:
         >>> f = smt.Function('f', smt.IntSort(), smt.IntSort())
         >>> x,y,z = smt.Ints('x y z')
         >>> E.add_term(f(x))
-        >>> E.ematch([smt.IntSort()], lambda v: (f(v), v + 1))
+        >>> E.rw([smt.IntSort()], lambda v: (f(v), v + 1))
         >>> assert E.find(f(x)) == E.find(x + 1)
 
         """
@@ -123,3 +149,26 @@ class EGraph:
             if self.in_terms(lhs):
                 self.add_term(rhs)
                 self.union(lhs, rhs)
+
+    def ematch(
+        self, vs: list[smt.ExprRef], pat: smt.ExprRef
+    ) -> list[list[smt.ExprRef]]:
+        """
+        Find all matches of pat in the egraph.
+
+        >>> E = EGraph()
+        >>> f = smt.Function('f', smt.IntSort(), smt.IntSort())
+        >>> x,y,z = smt.Ints('x y z')
+        >>> E.add_term(f(x))
+        >>> E.union(f(x), x)
+        >>> E.rebuild()
+        >>> E.ematch([y], f(f(y)))
+        [[x]]
+        """
+        res = []
+        for eids in itertools.product(*[self.roots[v.sort()] for v in vs]):
+            ts = [self.terms[eid] for eid in eids]
+            lhs = smt.substitute(pat, *zip(vs, ts))
+            if self.in_terms(lhs):
+                res.append(ts)
+        return res
