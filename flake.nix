f2870dc65e57518c15626190d5cd94a829c923e6
Graham <gleachkr@gmail.com>
2025-10-08 09:49:24 -0400

    Add a nix flake
    - The flake builds a uv2nix style venv:
      https://pyproject-nix.github.io/uv2nix/usage/getting-started.html
    
    - This should make it possible for nix development environments to include
      knuckledragger as a dependency
    
    This makes it possible to incorporate knuckledragger into toolsets
    exposed by nix development environments.

---
+++ flake.nix
@@ -0,0 +1,74 @@
+{
+  description = "Knuckledragger - A Low Barrier Proof Assistant";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      pyproject-nix,
+      uv2nix,
+      pyproject-build-systems,
+      ...
+    }:
+    let
+      inherit (nixpkgs) lib;
+      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+
+      workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+
+      overlay = workspace.mkPyprojectOverlay {
+        sourcePreference = "wheel";
+      };
+
+      pyprojectOverrides = final: prev: {
+        knuckledragger = prev.knuckledragger.overrideAttrs  {
+          patches = .nix/pyproject.patch;
+        };
+      };
+
+      pythonSets = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          python = pkgs.python312;
+        in
+        (pkgs.callPackage pyproject-nix.build.packages {
+          inherit python;
+        }).overrideScope
+          (
+            lib.composeManyExtensions [
+              pyproject-build-systems.overlays.wheel
+              overlay
+              pyprojectOverrides
+            ]
+          )
+      );
+
+    in
+    {
+      packages = forAllSystems (system: {
+        default = pythonSets.${system}.mkVirtualEnv "Knuckledragger-env" workspace.deps.default;
+      });
+    };
+}
