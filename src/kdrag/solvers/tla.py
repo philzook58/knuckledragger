b41000105435e5824089c87dfc64a50a8855739d
Philip Zucker <philzook58@gmail.com>
2026-07-02 16:36:41 -0400

    more tla calling

--- src/kdrag/solvers/tla.py
+++ src/kdrag/solvers/tla.py
@@ -6,9 +6,42 @@ import subprocess
 import xml.etree.ElementTree as ET
 from dataclasses import dataclass
 import kdrag.smt as smt
+import operator
 
 kdrag.solvers.download("https://github.com/tlaplus/tlaplus/releases/download/v1.8.0/tla2tools.jar", "tla2tools.jar", "237332bdcc79a35c7d26efa7b82c77c85c2744591c5598673a8a45085ff2a4fb")
 
+
+def check(filename : str):
+    """
+    Run the model checker on a tla file. Returns the stdout of the model checker.
+    """
+    return subprocess.run(
+        [
+            "java",
+            "-cp",
+            kdrag.solvers.binpath("tla2tools.jar"),
+            "tlc2.TLC",
+            filename,
+        ],
+        check=True,
+        capture_output=True,
+    ).stdout
+
+def pluscal_translate(filename : str) -> bytes:
+    """
+    Run the pluscal translator on a tla file. Returns the stdout of the translator.
+    """
+    return subprocess.run(
+        [
+            "java",
+            "-cp",
+            kdrag.solvers.binpath("tla2tools.jar"),
+            "pcal.trans",
+            filename,
+        ],
+        check=True,
+        capture_output=True,
+    ).stdout
 
 def tla_to_xml(filename):
     """
@@ -32,6 +65,7 @@ def tla_to_xml(filename):
 
 @dataclass
 class App:
+    "Untyped Ast for expressions"
     f: object
     args: list["App"]
 
@@ -40,82 +74,155 @@ class App:
             return f"{self.f}"
         else:
             return f"{self.f}({', '.join(map(str, self.args))})"
-        
+
+
+
+
 @dataclass
 class Module:
     name : str
     variables : list[str]
     definitions : dict[str, App]
+    theorems : list[App]
+    # assumes, constants, extends
+
+    def action(self, actname : str, decls: dict[str, smt.FuncDeclRef | smt.ExprRef]) -> smt.ExprRef:
+        """
+        Return the action of the module as an smt expression. These are expressions that may involve primed variables, 
+        but do not involve other temporal operators.
+        """
+        return to_smt(self.definitions[actname], decls, sort=smt.BoolSort())
+    
+    def behavior(self, name : str, decls : dict[str, smt.FuncDeclRef | smt.ExprRef]) -> smt.ExprRef:
+        """
+        Return a useful smt form of statements that involve non trivial temporal operators
+        """
+        raise NotImplementedError("behavior", name, decls)
+    
+    def infer_sorts(self, typeok="TypeOk") -> dict[str, smt.ExprRef]:
+        todo = [self.definitions[typeok]]
+        decls = {}
+        while todo:
+            e = todo.pop()
+            match e:
+                case App("\\land", args) | App("$ConjList", args):
+                    todo.extend(args)
+                #case App("=", [App(lhs, []), rhs]):
+                #    assert lhs not in decls, f"lhs {lhs} already in decls"
+                #    decls[lhs] = smt.Const(lhs, to_smt(rhs, decls).sort())
+                case App("\\in", [App(lhs, []), rhs]):
+                    assert lhs not in decls, f"lhs {lhs} already in decls"
+                    doms = smt.domains(to_smt(rhs, decls))
+                    assert len(doms) == 1, f"expected one domain for {rhs}, got {doms}"
+                    decls[lhs] = smt.Const(lhs, doms[0])
+                case _:
+                    raise ValueError("unexpected expression in infer_sorts", e)
+        return decls
 
     @staticmethod
-    def of_file(self, filename): ...
+    def of_file(filename) -> "Module":
+        mods = ET.fromstring(tla_to_xml(filename))
+        name = mods.findtext("RootModule")
+        assert name is not None
 
-def tla_exprs(filename) -> dict[str, App]:
-    mods = ET.fromstring(tla_to_xml(filename))
+        by_uid = {}
+        for entry in mods.findall("context/entry"):
+            uid = entry.findtext("UID")
+            node = next((c for c in entry if c.tag != "UID"), None)
+            if uid is not None and node is not None:
+                by_uid[uid] = node
 
-    by_uid = {}
-    entries = mods.findall("context/entry")
-    assert entries is not None, "No context entries found in XML output"
-    for entry in entries:
-        uid = entry.findtext("UID")
-        node = next((c for c in entry if c.tag != "UID"), None)
-        if uid is not None and node is not None:
-            by_uid[uid] = node
+        def ref_name(ref):
+            uid = ref.findtext("UID")
+            node = by_uid.get(uid)
+            if node is None:
+                return f"UID:{uid}"
+            return node.findtext("uniquename") or node.tag
 
-    def ref_name(ref):
-        uid = ref.findtext("UID")
-        node = by_uid.get(uid)
-        if node is None:
-            return f"UID:{uid}"
-        return node.findtext("uniquename") or node.tag
+        def expr(node):
+            if node.tag == "OpApplNode":
+                ref = next(iter(node.find("operator")), None)
+                f = ref_name(ref) if ref is not None else node.tag
+                operands = node.find("operands")
+                return App(f, [] if operands is None else [expr(arg) for arg in operands])
+            if node.tag.endswith("Ref"):
+                return App(ref_name(node), [])
+            if node.tag == "NumeralNode":
+                return App(int(node.findtext("IntValue")), [])
+            return App(node.findtext("uniquename") or node.tag, [])
 
-    def expr(node):
-        if node.tag == "OpApplNode":
-            ref = next(iter(node.find("operator")), None)
-            f = ref_name(ref) if ref is not None else node.tag
-            operands = node.find("operands")
-            args = [] if operands is None else [expr(arg) for arg in operands]
-            return App(f, args)
-        if node.tag.endswith("Ref"):
-            return App(ref_name(node), [])
-        if node.tag == "NumeralNode":
-            return App(int(node.findtext("IntValue")), [])
-        return App(node.findtext("uniquename") or node.tag, [])
+        root = next(
+            node
+            for node in by_uid.values()
+            if node.tag == "ModuleNode" and node.findtext("uniquename") == name
+        )
+        variables = []
+        for ref in root.findall("OpDeclNodeRef"):
+            node = by_uid.get(ref.findtext("UID"))
+            if node is not None and node.findtext("./location/filename") == name:
+                variables.append(node.findtext("uniquename"))
 
-    res = {}
-    for node in by_uid.values():
-        if node.tag != "UserDefinedOpKind":
-            continue
-        if node.findtext("./location/filename") != mods.findtext("RootModule"):
-            continue
-        body = node.find("./body/*")
-        if body is not None:
-            res[node.findtext("uniquename")] = expr(body)
-    return res
+        definitions = {}
+        theorems = []
+        for node in by_uid.values():
+            if node.findtext("./location/filename") != name:
+                continue
+            body = node.find("./body/*")
+            if node.tag == "UserDefinedOpKind" and body is not None:
+                definitions[node.findtext("uniquename")] = expr(body)
+            elif node.tag == "TheoremNode" and body is not None:
+                theorems.append(expr(body))
+
+        return Module(name, variables, definitions, theorems)
 
 
-def to_smt(e: App, decls: dict[object, smt.FuncDeclRef], sort=None):
+
+
+binops = {
+    "+": operator.add,
+    "-": operator.sub,
+    "*": operator.mul,
+    "%" : operator.mod,
+
+}
+
+
+def to_smt(e: App, decls: dict[str, smt.FuncDeclRef | smt.ExprRef], sort=None):
     if e.f in decls:
+        assert isinstance(e.f, str)
         f = decls[e.f]
-        assert len(e.args) == f.arity()
-        assert sort is None or sort == f.range()
-        args = [to_smt(arg, decls, sort=f.domain(i)) for i, arg in enumerate(e.args)]
-        return f(*args)
+        if isinstance(f, smt.FuncDeclRef):
+            assert len(e.args) == f.arity()
+            assert sort is None or sort == f.range()
+            args = [to_smt(arg, decls, sort=f.domain(i)) for i, arg in enumerate(e.args)]
+            return f(*args)
+        elif isinstance(f, smt.ExprRef):
+            assert not e.args
+            assert sort is None or sort == f.sort()
+            return f
+        else:
+            raise ValueError(f"decls[{e.f}] is not a FuncDeclRef or ExprRef")
     elif isinstance(e.f, int) and not e.args:
         assert sort is None or sort == smt.IntSort()
         return smt.IntVal(e.f)
-    elif e.f == "\\land":
-        assert len(e.args) >= 2
+    elif e.f == "\\land" or e.f == "$ConjList":
         args = [to_smt(arg, decls, sort=smt.BoolSort()) for arg in e.args]
-        return smt.And(*args)
+        if len(args) == 0:
+            return smt.BoolVal(True)
+        elif len(args) == 1:
+            return args[0]
+        else:
+            return smt.And(*args)
     elif e.f == "\\lor":
         assert len(e.args) >= 2
         args = [to_smt(arg, decls, sort=smt.BoolSort()) for arg in e.args]
         return smt.Or(*args)
     elif e.f == "=":
         assert len(e.args) == 2
-        args = [to_smt(arg, decls) for arg in e.args]
-        return smt.Eq(args[0], args[1])
+        # maybe vice versa for sort propagation?
+        rhs = to_smt(e.args[1], decls)
+        lhs = to_smt(e.args[0], decls, sort=rhs.sort())
+        return smt.Eq(lhs, rhs)
     elif e.f == "..":
         assert len(e.args) == 2
         args = [to_smt(arg, decls, sort=smt.IntSort()) for arg in e.args]
@@ -139,11 +246,14 @@ def to_smt(e: App, decls: dict[object, smt.FuncDeclRef], sort=None):
         assert len(e.args) == 2
         args = [to_smt(arg, decls, sort=sort) for arg in e.args]
         return args[0] + args[1]
+    elif e.f == "BOOLEAN":
+        assert not e.args
+        return smt.FullSet(smt.BoolSort())
     # These appear in spec statements
-    elif e.f == "$SquareAct":
-        return smt.Const("$SQUAREACT TODO", smt.BoolSort())
-    elif e.f == "[]":
-        return smt.Const("[] TODO", smt.BoolSort())
+    #elif e.f == "$SquareAct":
+    #    return smt.Const("$SQUAREACT TODO", smt.BoolSort())
+    #elif e.f == "[]":
+    #    return smt.Const("[] TODO", smt.BoolSort())
     # elif sort is not None:
     #    # fallback to uninterprted function
     #    args = [to_smt(arg, decls) for arg in e.args]
--- tests/test_tla.py
+++ tests/test_tla.py
@@ -12,22 +12,60 @@ HCnxt == hr' = IF hr = 12 THEN 1 ELSE hr + 1
           (* Alternately expressed as: hr' = (hr % 12) + 1 *)
 
 HC == HCini /\ [][HCnxt]_hr
+
+------------------------------
+
+THEOREM  HC => []HCini
+
 ==========================
+"""
+
+hour_cfg = r"""
+SPECIFICATION HC
+   \* This statement tells TLC that HC is the specification that it
+   \* should check.
+
+INVARIANT HCini
 """
 
 def test_tla_to_xml():
     with open("/tmp/HourClock.tla", "w") as f:
         f.write(hour_clock)
     xml = tla.tla_to_xml("/tmp/HourClock.tla")
-    exprs = tla.tla_exprs("/tmp/HourClock.tla")
     mod = tla.Module.of_file("/tmp/HourClock.tla")
     assert mod.name == "HourClock"
     assert mod.variables == ["hr"]
-    assert mod.definitions.keys() == ["HCini", "HCnxt", "HC"]
-    decls : dict[object, smt.FuncDeclRef] = {"hr": smt.Function("hr", smt.IntSort())}
-    for name, e in exprs.items():
-        print(f"{name} = {e}")
-        ze = tla.to_smt(e, decls, sort=None)
-        print(decls)
-        decls[name] = smt.Function(name, ze.sort())
-        print(ze)
+    assert list(mod.definitions.keys()) == ["HCini", "HCnxt", "HC"]
+    assert mod.theorems == [tla.App("=>", [tla.App("HC", []), tla.App("[]", [tla.App("HCini", [])])])]
+    hr, hr1 = smt.Ints("hr hr'")
+    decls = {"hr": hr}
+    assert mod.action("HCini", decls).sort() == smt.BoolSort()
+    assert mod.action("HCnxt", decls).eq(hr1 == smt.If(hr == 12, 1, hr + 1))
+    assert mod.infer_sorts(typeok="HCini") == decls
+    with open("/tmp/HourClock.cfg", "w") as f:
+        f.write(hour_cfg)
+    tla.check("/tmp/HourClock.tla")
+
+
+
+mytest = r"""
+---- MODULE MyTest ----
+VARIABLES is_unique
+VARIABLES biz
+
+TypeInvariant == /\ is_unique \in BOOLEAN
+
+==========================
+"""
+
+def test_tla_mytest():
+    with open("/tmp/MyTest.tla", "w") as f:
+        f.write(mytest)
+    mod = tla.Module.of_file("/tmp/MyTest.tla")
+    assert mod.name == "MyTest"
+    assert mod.variables == ["biz", "is_unique"]
+    assert list(mod.definitions.keys()) == ["TypeInvariant"]
+    assert mod.theorems == []
+    decls = mod.infer_sorts(typeok="TypeInvariant")
+    assert decls == {"is_unique": smt.Const("is_unique", smt.BoolSort())}
+    assert mod.action("TypeInvariant", decls).sort() == smt.BoolSort()
